e82be36f9c6c14ff9553246b26f34a9e286d9bb6
Vitor Linaris <vitor.linaris@gmail.com>
2024-08-24 13:16:51 -0300

    feat: created theme color

---
+++ src/theme/theme.color.ts
@@ -0,0 +1,21 @@
+const  Colors = {
+    brand: {
+        brandColorRed:  '#EB3850'
+    },
+    gradient: {
+        gradientColorRed: 'linear-gradient(270deg, #A93E4C 0%, #EB3850 100%)',
+        gradientColorYellow: 'linear-gradient(270deg, #D6AE7B 0%, #E3C79E 100%);',
+        gradientColorDark: 'linear-gradient(270deg, #29323C 0%, #485563 100%)'
+    },
+    dark: {
+        dark: '#1A191E',
+        gray: '#47464A'
+    },
+    system: {
+        message: '#2962FF',
+        success: '#0BB07B',
+        warning: '#FFCE52',
+        error: '#F03D3D',
+        information: '#E0E0E0'
+    }
+}
